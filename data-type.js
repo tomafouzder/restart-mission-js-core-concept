555c866cef7eb6df589986f6cef57191253d4c54
tomafouzder <tomafouzder12@gmail.com>
2026-02-06 05:10:14 +0600

    first commit

---
+++ data-type.js
@@ -0,0 +1,24 @@
+// Data type: Js is a dynamic typed programming language 
+let data = 4;
+data = 'andor killa bandorbon with some katabon';
+data = false;
+data = { name: 'bdrul', address: 'borsa ghat', study: 'botany', studentId: 15 }
+data = ['Tom', 'Jom', 'Kom', 'Bob']
+data = undefined;
+data = null;
+
+console.log(typeof data);
+
+// Js: Primitive data type
+const price = 5245; //number
+const name = 'Bob'; //string
+const isPoor = false; // boolean
+let isLocal = undefined; // usually you should not set value to undefine 
+let isKnown;
+const merit = null
+console.log(isKnown, merit);
+
+
+// Non-Primitive data type
+const student = { name: 'badal', location: 'bristi goli', subject: 'botany', studentId: 4556 }   // Object
+const friend = ['Tom', 'Bob', 'jack', 'rock']  // array
