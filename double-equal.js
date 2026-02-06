1bf17e1a3f28abd444de68e563709e23edf45a14
tomafouzder <tomafouzder12@gmail.com>
2026-02-06 06:24:14 +0600

    added this double equal and triple equal basic concept

---
+++ double-equal.js
@@ -0,0 +1,16 @@
+// double equal (==)----> type convert 
+// console.log(2 == 2)  // true 
+// console.log(2 == '2')  // true 
+
+// false
+// console.log(1 === 2)
+// console.log(true == 1)
+// console.log(true == '1')
+// console.log(false == 0)
+// console.log(false == '0')
+// console.log(null == undefined)
+// console.log(0 == '')
+// console.log([] == '')
+console.log([] == [])
+console.log([5] == '5')
+console.log({} == {})
---
+++ triple-equal.js
@@ -0,0 +1,19 @@
+/**
+ ************** Triple Equal  ****************
+ * ------------------------------------------*
+ * first check the type. if type is different then , they are different 
+ * if type is same then it check the value. if value is same -->true, if value is false then ---->false
+ * 
+ */
+
+ 
+console.log(2 === 2)  // true 
+
+// false
+console.log(2 === '2');
+console.log(1 === 2)
+console.log(true === 1)
+console.log(false === 0)
+console.log(null === undefined)
+console.log(0 === '')
+console.log([] === '')
